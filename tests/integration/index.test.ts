95fb2ff7918760f551b45ee4aeb8fb3d3548c035
Kakkarrot <lqf5474@gmail.com>
2026-04-08 17:28:32 -0700

    Narrow active integration suite

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -5,7 +5,3 @@ registerIntegrationSuiteHooks();
 import "./auth.session.test";
 import "./bookings.test";
 import "./chats.test";
-import "./discover.test";
-import "./realtime-stream.test";
-import "./schema-contract.test";
-import "./users.test";
